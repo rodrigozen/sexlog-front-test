6cb2ab4e8b6a1a15299001b59986649d2d50bc07
Rodrigo Zen <rodrigomzen@gmail.com>
2017-07-09 00:55:19 -0300

    Add steps element

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -3,18 +3,22 @@
         <button @click="openModal">Assine conteúdo VIP</button>
         <v-modal v-if="showModal" @close="closeModal">
             <h3 slot="header">Assine o Sexlog VIP</h3>
+            <div slot="body" class='content'>
+                <ul id="stepsAssinatura" class="steps">
+                    <li class="step is-active">Escolha o plano ideal pra você</li>
+                    <li class="step">Escolha a forma de pagamento</li>
+                </ul>
+            </div>
         </v-modal>
     </div>
 </template>
 
 <script>
-// import Modal from '@/components/Modal';
-
 export default {
     name: 'home',
     data() {
         return {
-            showModal: false,
+            showModal: true,
         };
     },
     methods: {
@@ -27,3 +31,12 @@ export default {
     },
 };
 </script>
+
+<style scoped lang="scss">
+@import '../styles/vendors/mq';
+#stepsAssinatura {
+    @include mq($until: tablet) {
+        height: 0;
+    }
+}
+</style>
