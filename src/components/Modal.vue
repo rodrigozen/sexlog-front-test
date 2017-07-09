8641c272e5e6c44758b2892536b5a2a7bc955721
Rodrigo Zen <rodrigomzen@gmail.com>
2017-07-08 22:48:45 -0300

    Add base modal component

---
+++ src/components/Modal.vue
@@ -0,0 +1,36 @@
+<template lang="html">
+    <transition name="modal">
+        <div class="modal-mask">
+            <div class="modal-wrapper">
+                <div class="modal-container">
+                    <div class="modal-header">
+                        <div class="modal-title">
+                            <slot name="header"></slot>
+                        </div>
+                        <div class="modal-close is-clicable" @click="$emit('close')">
+                            <svg viewbox="0 0 10 10" width="15" height="15">
+                              <path class="close-x" d="M 5,5 L 10,10 M 10,5 L 5,10" />
+                            </svg>
+                        </div>
+                    </div>
+                    <div class="modal-body">
+                        <slot name="body"></slot>
+                    </div>
+                    <div class="modal-footer">
+                        <slot name="footer">
+
+                        </slot>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </transition>
+</template>
+
+<script>
+export default {
+    data() {
+        return {};
+    },
+};
+</script>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,13 +1,29 @@
 <template>
     <div class="home">
+        <button @click="openModal">Assine conteúdo VIP</button>
+        <v-modal v-if="showModal" @close="closeModal">
+            <h3 slot="header">Assine o Sexlog VIP</h3>
+        </v-modal>
     </div>
 </template>
 
 <script>
+// import Modal from '@/components/Modal';
+
 export default {
     name: 'home',
     data() {
-        return {};
+        return {
+            showModal: false,
+        };
+    },
+    methods: {
+        openModal() {
+            this.showModal = true;
+        },
+        closeModal() {
+            this.showModal = false;
+        },
     },
 };
 </script>
