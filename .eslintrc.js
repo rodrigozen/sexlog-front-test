a5dcf315e617f09ea4245a7c0549b6b62b25c549
Rodrigo Zen <rodrigomzen@gmail.com>
2017-07-10 22:18:46 -0300

    Install babel polyfill and json loader

--- .eslintrc.js
+++ .eslintrc.js
@@ -36,7 +36,8 @@ module.exports = {
     // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
 
-    "indent": ["error", 4]
+    "indent": ["error", 4],
+    "import/no-webpack-loader-syntax": "off"
 
   }
 }
